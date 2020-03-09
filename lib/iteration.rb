e8f4ea8aba7b17d753a35ce66ccc20b9514a0c21
Kathryn Clark <kathrynlael@gmail.com>
2020-03-09 15:41:54 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -25,6 +25,8 @@ def total_even_pairs(src)
     if (src[i][0] % 2 == 0) && (src[i][1] % 2 == 0)
       total += (src[i][0] + src[i][1])
   end 
+   i+=1 
+ end
   total
   
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
